1d3f729773031b1956c921e2f70635dc31b44253
Eugene Zubkov <eugen.zubkov@moovweb.com>
2019-10-16 12:13:47 +0300

    убирает transform с активного состояния кнопки меню

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,6 +2,5 @@ const navigationPages = document.querySelector('.navigation__pages');
 const navigationButton = document.querySelector('.navigation__button');
 
 navigationButton.addEventListener('click', event => {
-    event.target.classList.toggle('navigation__button--active');
     navigationPages.classList.toggle('navigation__pages--active');
 });
