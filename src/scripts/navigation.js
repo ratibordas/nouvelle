134b396d587deac52a34d92d31a3389daa8531b3
Eugene Zubkov <eugen.zubkov@moovweb.com>
2019-10-17 18:22:59 +0300

    добавляет атрибут состояния меню для кнопки меню

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,6 +1,28 @@
 const navigationPages = document.querySelector('.navigation__pages');
 const navigationButton = document.querySelector('.navigation__button');
+let isMenuHidden = false;
+
+const isMenuHiddenCheck = () => {
+  let attributeValue = navigationButton.getAttribute("aria-expanded");
+
+  attributeValue === 'false' ? isMenuHidden = false : isMenuHidden = true;
+
+  return isMenuHidden;
+}
+
+const handleNavigationMenuState = () => {
+    if (isMenuHiddenCheck()) {
+      navigationButton.setAttribute("aria-expanded", "false");
+      return;
+    } 
+    navigationButton.setAttribute("aria-expanded", "true");
+};
+
+const showHideNavigationMenu = () => {
+    navigationPages.classList.toggle('navigation__pages--active');
+};
 
 navigationButton.addEventListener('click', event => {
-    navigationPages.classList.toggle('navigation__pages--active');
+    handleNavigationMenuState();
+    showHideNavigationMenu();
 });
